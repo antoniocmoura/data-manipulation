3268afa71dd5c7469c8dff922c50732c2822eda5
Antonio Carlos de Moura <antoniocmoura@gmail.com>
2022-04-03 19:48:28 -0300

    manipulação de dados utilizando Map, Reduce, Filter e ForEarch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import motos from './motos'
+import React from 'react'
+
+let motos_por_marca = []
+let idx_marca = -1;
+
+motos.forEach(moto => {
+  idx_marca = motos_por_marca.findIndex(item => item.marca === moto.marca)
+  if (idx_marca >= 0) {
+    motos_por_marca[idx_marca].quantidade = motos_por_marca[idx_marca].quantidade + 1
+  }
+  else {
+    let item = {
+      marca: moto.marca,
+      quantidade: 1
+    }
+    motos_por_marca.push(item)
+  }
+})
+
+const qtde_motos_por_marca = motos_por_marca.map( item => {
+  return item.marca + ' (' + item.quantidade + ')'
+})
+
+const total_km = motos.reduce((acumulado, moto) => {
+  if (acumulado.km) {
+    return acumulado.km + moto.km
+  } else {
+    return acumulado + moto.km
+  }
+})
+
+const motos_zero_km = motos.filter(moto => moto.km === 0).map(moto => moto.marca + ' ' + moto.modelo)
+
+const listagem_motos = motos.map((moto, index) => {
+  return (
+    <tr key={index}>
+      <td>{moto.marca}</td>
+      <td>{moto.modelo}</td>
+      <td>{moto.cilindrada}</td>
+      <td>{moto.ano}</td>
+      <td>{moto.km}</td>
+      <td>{moto.cor}</td>
+    </tr>
+  )
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          <tr>
+            <th colSpan="6">LISTAGEM DE MOTOS</th>
+          </tr>
+          <tr>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Cilindrada</th>
+            <th>Ano</th>
+            <th>KM</th>
+            <th>Cor</th>
+          </tr>
+          {listagem_motos}
+          <tr>
+            <td colSpan="6">Soma da quilometragem das motos: {total_km} </td>
+          </tr>
+          <tr>
+            <td colSpan="6">Motos zero km: {motos_zero_km.join(', ')} </td>
+          </tr>
+          <tr>
+            <td colSpan="6">Quantidade de motos por marca: {qtde_motos_por_marca.join(', ')} </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/motos.js
@@ -0,0 +1,60 @@
+let motos = [
+    {
+       marca: 'Honda',
+       modelo: 'CG',
+       cilindrada: 125, 
+       ano: 2015,
+       km: 75000,
+       cor: 'vermelha'
+    },
+    {
+        marca: 'Honda',
+        modelo: 'Hornet',
+        cilindrada: 600, 
+        ano: 2021,
+        km: 12000,
+        cor: 'branca'
+     },
+     {
+        marca: 'Yamaha',
+        modelo: 'Fazer',
+        cilindrada: 150, 
+        ano: 2020,
+        km: 5720,
+        cor: 'preta'
+     },
+     {
+        marca: 'Yamaha',
+        modelo: 'Lander',
+        cilindrada: 250, 
+        ano: 2022,
+        km: 1500,
+        cor: 'azul'
+     },
+     {
+        marca: 'Yamaha',
+        modelo: 'XT 660',
+        cilindrada: 660, 
+        ano: 2009,
+        km: 22000,
+        cor: 'vermelha'
+     },
+     {
+        marca: 'BMW',
+        modelo: 'R 1250 GS',
+        cilindrada: 1254, 
+        ano: 2022,
+        km: 0,
+        cor: 'branca'
+     },
+     {
+        marca: 'Harley Davidson',
+        modelo: 'Fat Boy',
+        cilindrada: 1690, 
+        ano: 2022,
+        km: 0,
+        cor: 'preta'
+     },
+]
+
+export default motos;
